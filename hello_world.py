63dcb1d9d84b15b42a3878292ac07eb9fd5f18d7
Sage <sage.halewolfe@gmail.com>
2023-02-07 15:51:01 -0500

    First Commit!

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+
+my_list = ["I would ", "walk", 500, "miles", True]
+
+print(my_list[4])
+
+my_dict = {'monkey':'banana', 'geko':'mealworm'}
+
+print(my_dict['geko'])
+
+never_gonna = ("Give you up", "Let you down", "Run around", "Desert you")
+
+print(never_gonna[3])
